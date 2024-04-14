cdeaa522b6b93287514a60d62f0c8565ae6fcad8
Viktoriia46 <132668579+Viktoriia46@users.noreply.github.com>
2024-04-15 01:34:15 +0300

    Add files via upload

---
+++ Laba2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x, y, z;
+
+        Console.WriteLine("Enter coordinates for the point in 3D space:");
+        Console.Write("X: ");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("Y: ");
+        y = int.Parse(Console.ReadLine());
+        Console.Write("Z: ");
+        z = int.Parse(Console.ReadLine());
+
+        Point3D point = new Point3D(x, y, z);
+
+        if (point.AreCoordinatesInteger())
+        {
+            Console.WriteLine("All coordinates are integers.");
+        }
+        else
+        {
+            Console.WriteLine("Not all coordinates are integers.");
+        }
+
+        if (point.IsInFirstOctant())
+        {
+            Console.WriteLine("The point is in the first octant.");
+        }
+        else
+        {
+            Console.WriteLine("The point is not in the first octant.");
+        }
+    }
+}
